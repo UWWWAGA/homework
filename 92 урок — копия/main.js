78bb4927b476dd971916d9088d4b87b139a9aa0f
UWWWAGA <147951254+UWWWAGA@users.noreply.github.com>
2023-11-29 01:05:34 +0300

    Add files via upload

---
+++ 92 урок — копия/main.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector('.modal__btn--open')
+const modal = document.querySelector('.modal')
+const body = document.body
+
+const openModal = () => {
+  modal.classList.add('modal--open')
+  body.classList.add('body--fixed')
+}
+
+const closeModel = () => {
+  modal.classList.remove('modal--open')
+  body.classList.remove('body--fixed')
+}
+
+btn.addEventListener('click', openModal)
+
+modal.addEventListener('click', event => {
+  const target = event.target
+  if (target && target.classList.contains('modal') || target.classList.contains('modal__btn--close')) {
+    closeModel()
+  }
+})
+
+document.addEventListener('keydown', event => {
+  if (event.code === 'Escape' && modal.classList.contains('modal--open')) {
+    closeModel()
+  }
+})
+
+
+
+
+
+
+
+
